54cee3b6060b162d1f9c7993a5a91f0101074977
Timothy Alexis Vass <timvas@bredband2.se>
2024-05-06 23:03:45 +0200

    Fix level selection

--- scripts/level_selection.gd
+++ scripts/level_selection.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
+var level_button_scene = preload("res://scenes/level_button.tscn")
 var level_scene = preload("res://scenes/level.tscn")
 var previous_x_position = 200
 var line
 var rng = RandomNumberGenerator.new()
-
-var level_buttons = []
 
 const LINE_OFFSET = Vector2(100, 85)
 
@@ -16,13 +15,12 @@ func _ready():
 	var click_mask = BitMap.new()
 	click_mask.create_from_image_alpha(button.texture_normal.diffuse_texture.get_image())
 	button.texture_click_mask = click_mask
-	level_buttons.append(button)
 
 	for i in range(2, Spawn.WAVE_TIER.size()+1):
 		line = $Lines/Line2D.duplicate()
 		line.set_visible(true)
 		$Lines.add_child(line)
-		button = $Levels/LevelButton.duplicate()
+		button = level_button_scene.instantiate()
 		$Levels.add_child(button)
 		button.level = i
 		
@@ -35,13 +33,11 @@ func _ready():
 		button.get_node("Label").set_text(str(i))
 		button.get_node("Label").modulate = Color(0.2, 0.2, 0.2)
 		button.get_node("BackGlow").modulate = Color(0.2, 0.2, 0.2)
-		level_buttons.append(button)
 
-	for level_button in level_buttons:
-		level_button.connect("pressed", Callable(self, "play_level").bind(button))
+	for level_button in $Levels.get_children():
+		level_button.connect("pressed", Callable(self, "play_level").bind(level_button))
 
 func play_level(button):
-	print("hello")
 	var level_instance = level_scene.instantiate()
 	level_instance.set("number", button.level)
 	get_parent().add_child(level_instance)
