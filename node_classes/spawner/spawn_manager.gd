837af0a3d5033204e6656362b28421362d6d3a26
Timothy Alexis Vass <timvas@bredband2.se>
2024-05-20 10:41:15 +0200

    Fix Yorran sprites

--- node_classes/spawner/spawn_manager.gd
+++ node_classes/spawner/spawn_manager.gd
@@ -7,7 +7,7 @@ var wave = 0
 
 var ships = {
 	1: {
-		"Xenor": preload("res://scenes/ships/1/Xenor.tscn"),
+		"Yorran": preload("res://scenes/ships/1/Yorran.tscn"),
 	},
 	2: {
 		"Sabre": preload("res://scenes/ships/1/Sabre.tscn"),
@@ -25,12 +25,12 @@ var ships = {
 		"Vusion": preload("res://scenes/ships/1/Vusion.tscn"),
 		"Wail": preload("res://scenes/ships/1/Wail.tscn"),
 		"Weruna": preload("res://scenes/ships/1/Weruna.tscn"),
+		"Xenor": preload("res://scenes/ships/1/Xenor.tscn"),
 		
 		"Garra": preload("res://scenes/ships/2/Garra.tscn"),
 		"Sabir": preload("res://scenes/ships/2/Sabir.tscn"),
 	},
 	3: {
-		"Yorran": preload("res://scenes/ships/1/Yorran.tscn"),
 		"Yorria": preload("res://scenes/ships/1/Yorria.tscn"),
 		"Zaryan": preload("res://scenes/ships/1/Vallin.tscn"),
 		"Zaryshan": preload("res://scenes/ships/1/Zaryshan.tscn"),
